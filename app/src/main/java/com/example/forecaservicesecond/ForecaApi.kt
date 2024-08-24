915b16f7301e6a6b1507cfaa38b136119a3d9832
Michel-mihim <ya-mihim@yandex.ru>
2024-08-24 11:58:40 +0300

    1_code_combed

--- app/src/main/java/com/example/forecaservicesecond/ForecaApi.kt
+++ app/src/main/java/com/example/forecaservicesecond/ForecaApi.kt
@@ -10,12 +10,20 @@ import retrofit2.http.Path
 interface ForecaApi {
 
     @POST("/authorize/token?expire_hours=-1")
-    fun authenticate(@Body request: ForecaAuthRequest): Call<ForecaAuthResponse>
+    fun authenticate(
+        @Body request: ForecaAuthRequest
+    ): Call<ForecaAuthResponse>
 
     @GET("/api/v1/location/search/{query}")
-    fun getLocations(@Header("Authorization") token: String, @Path("query") query: String): Call<LocationsResponse>
+    fun getLocations(
+        @Header("Authorization") token: String,
+        @Path("query") query: String
+    ): Call<LocationsResponse>
 
     @GET("/api/v1/current/{location}")
-    fun getForecast(@Header("Authorization") token: String, @Path("location") locationId: Int): Call<ForecastResponse>
+    fun getForecast(
+        @Header("Authorization") token: String,
+        @Path("location") locationId: Int
+    ): Call<ForecastResponse>
 
 }
